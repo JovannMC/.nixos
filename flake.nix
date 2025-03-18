1213afeaf565d7a9cd70a0c7e393966e8df27787
JovannMC <jovannmc@femboyfurry.net>
2025-03-18 16:16:04 +0300

    Initial commit
    Co-authored-by: AnnoyingRains <avali@avali.zone>

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "System flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = { 
+      joebox = nixpkgs.lib.nixosSystem rec {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/joebox/configuration.nix
+        ];
+      };
+    };
+    
+
+  };
+}
