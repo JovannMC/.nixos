2c96d48a972b50363ec3290ecb397f3e055e13f2
Maya <jovannmc@femboyfurry.net>
2026-05-13 13:31:25 +0300

    feat: macbook files

--- hosts/mayabook/configuration.nix
+++ hosts/mayabook/configuration.nix
@@ -4,17 +4,62 @@
 
 {
   config,
+  lib,
   pkgs,
-  inputs,
   ...
 }:
 
 {
+  imports = [
+    ./hardware-configuration.nix
+    ./apple-silicon-support
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = false;
+
+  networking.hostName = "mayabook-nixos"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
   # Implicitly enables IWD and disables wpa_supplicant
   networking.networkmanager.wifi.backend = "iwd";
 
-  # hardware.apple.touchBar = {
-  #   enable = true;
-  #   package = pkgs.tiny-dfr;
-  # };
+  # Set your time zone.
+  time.timeZone = "Asia/Qatar";
+
+  # Specify path to peripheral firmware files.
+  hardware.asahi.peripheralFirmwareDirectory = ./firmware;
+  # Or disable extraction and management of them completely.
+  #hardware.asahi.extractPeripheralFirmware = false;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+
 }
